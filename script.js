6469dd9afd5764b59dcb8027a08e22c895b55e66
aletankindness <aletkolt@gmail.com>
2017-07-26 23:40:30 +0100

    Javascript code working fine for getting location and setting the values on HTML only Fahrenheit to Celsius has not been implemented. Left is the interface and temp conversion.

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-var location = document.getElementById("");
-var weatherIcon = document.getElementById("");
-var button = document.getElementById("test-button");
-$(document).ready(function(){
-    button.addEventListener("click", getLocationWeather(35, 127));
-});
+var locatio = document.getElementById("location");
+var pressur = document.getElementById("pressure");
+var humidit = document.getElementById("humidity");
+var weathe = document.getElementById("weather");
+var temperatur = document.getElementById("temperature");
+var weatherIcon = document.getElementById("weather-icon");
+
+function geolocator(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition, errorOnGeolocator);
+    }
+    else{
+        console.log("Location services is not available");
+    }
+}
+
+function showPosition(position){
+    getLocationWeather(position.coords.latitude, position.coords.longitude);
+}
+
+function errorOnGeolocator(error){
+    console.log("An error has occurred");
+}
 
 function getLocationWeather(latitude, longitude){
     $.ajax({
@@ -18,13 +35,22 @@ function getLocationWeather(latitude, longitude){
 
     .done(update)
     .fail(errorHandler);
-
 }
 
 function update(response){
-    console.log(JSON.stringify(response));
+    console.log(response);
+    locatio.innerText = response.name;
+    weathe.innerText = response.weather[0].main;
+    weatherIcon.src = response.weather[0].icon;
+    pressur.innerText = response.main.pressure;
+    humidit.innerText = response.main.humidity;
+    temperatur.innerText = response.main.temp;
 }
 
 function errorHandler(jqXHR, textStatus, errorThrown ){
-
+    console.log("AJAX api failed and is not available");
 }
+
+$(document).ready(function(){
+    geolocator();
+});
